3f54bdc3e6cc55a0475c68d8b6a7905e39299f0e
WDrijver <willemdrijver@mac.com>
2026-01-04 21:06:33 +0100

    Minoir

--- GCC-6.50.sh
+++ GCC-6.50.sh
@@ -55,7 +55,7 @@ echo -e -n "\e[0m\e[36mDrop Prefix | "
 make drop-prefix PREFIX=$PREFIX >>$LOGFILES/part4.log 2>>$LOGFILES/part4_err.log
 echo -e -n "\e[0m\e[36mClone Repos (>1 min) | "
 make update $CPU NDK=3.2 PREFIX=$PREFIX >>$LOGFILES/part4.log 2>>$LOGFILES/part4_err.log
-mv projects/gcc-stable gcc
+mv projects/gcc-stable projects/gcc
 echo -e -n "\e[0m\e[36mBuild Compiler (>5 min) | "
 make all $CPU NDK=3.2 PREFIX=$PREFIX >>$LOGFILES/part4.log 2>>$LOGFILES/part4_err.log
 echo -e "\e[0m\e[36mAdd LibDebug\e[0m]"
