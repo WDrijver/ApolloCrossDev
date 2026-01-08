9cfd7fe509e69d7b5e61f061d617702589a71fcd
WDrijver <willemdrijver@mac.com>
2026-01-08 22:52:03 +0100

    STable and Latest released

---
+++ GCC-6.50-Latest.sh
@@ -0,0 +1,24 @@
+# ApolloCrossDev Primary Install Script v1.0
+
+VERSION=1.0
+CPU=-j16
+
+WORKSPACE="`pwd`"
+COMPILERS=Compilers
+PROJECTS=Projects
+COMPILER=GCC-6.50-Latest
+TARGET=m68k-amigaos
+PREFIX=$WORKSPACE/$COMPILERS/$COMPILER
+
+MASTER=/home/willem/ApolloCrossDev.Sources/amiga-gcc
+BRANCH=amiga-gcc-latest
+
+ARCHIVES=$WORKSPACE/$COMPILERS/_archives
+LOGFILES=$PREFIX/_logs
+BUILDS=$PREFIX/_builds
+SOURCES=$PREFIX/_sources
+
+export PATH=$PREFIX/bin:$PATH
+
+source GCC-Install.sh
+
